472f6943575e92639a8299c1404a072686fc2d17
Breichen1 <reichenmiikka@gmail.com>
2023-11-10 20:58:47 -0600

    Initial Commit
    Offloading initial files, including code from 364 BaseFalconSwerve and 3512 Swervebot-2022.

---
+++ src/main/java/frc/robot/CTREConfigs.java
@@ -0,0 +1,42 @@
+package frc.robot;
+
+
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+import com.ctre.phoenix.sensors.AbsoluteSensorRange;
+import com.ctre.phoenix.sensors.CANCoderConfiguration;
+import com.ctre.phoenix.sensors.SensorInitializationStrategy;
+import com.ctre.phoenix.sensors.SensorTimeBase;
+
+public final class CTREConfigs {
+    public TalonFXConfiguration swerveAngleFXConfig;
+    public TalonFXConfiguration swerveDriveFXConfig;
+    public CANCoderConfiguration swerveCanCoderConfig;
+
+    public CTREConfigs(){
+        swerveAngleFXConfig = new TalonFXConfiguration();
+        swerveDriveFXConfig = new TalonFXConfiguration();
+        swerveCanCoderConfig = new CANCoderConfiguration();
+
+        /* Swerve Angle Motor Configurations */
+       // SupplyCurrentLimitConfiguration angleSupplyLimit = new SupplyCurrentLimitConfiguration(
+         //   Constants.Swerve.angleEnableCurrentLimit, 
+         //   Constants.Swerve.angleContinuousCurrentLimit, 
+         //   onstants.Swerve.anglePeakCurrentLimit, 
+         
+      
+
+        /* Swerve Drive Motor Configuration */
+       
+
+              
+      
+        swerveDriveFXConfig.openloopRamp = Constants.Swerve.openLoopRamp;
+        swerveDriveFXConfig.closedloopRamp = Constants.Swerve.closedLoopRamp;
+        
+        /* Swerve CANCoder Configuration */
+        swerveCanCoderConfig.absoluteSensorRange = AbsoluteSensorRange.Unsigned_0_to_360;
+        swerveCanCoderConfig.sensorDirection = Constants.Swerve.canCoderInvert;
+        swerveCanCoderConfig.initializationStrategy = SensorInitializationStrategy.BootToAbsolutePosition;
+        swerveCanCoderConfig.sensorTimeBase = SensorTimeBase.PerSecond;
+    }
+}
